7b5c6b9a7f6fb04896bf8cc2c6395bf4590c4949
Sandeep Bhat <sandyethadka@gmail.com>
2018-03-20 22:24:47 +0530

    Battle component/route done

--- app/components/App.js
+++ app/components/App.js
@@ -3,16 +3,28 @@ var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Popular = require('./Popular');
 
 class App extends React.Component {
     render() {
         return (
             <Router>
                 <div className='container'>
-                  <Nav />
-                  <Route path='/popular' component={Popular} />
-                </div>
+                <Nav />
+
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/battle' component={Battle} />
+                    <Route path='/popular' component={Popular} />
+                    <Route render={function () {
+                    return <p>Not Found</p>
+                    }} />
+                </Switch>
+            </div>
             </Router>
         )
     }    
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,11 @@
 var React = require('react');
+var Link = require('react-router-dom').Link;
 
 class Home extends React.Component {
     render() {
         return (
             <div className='home-container'>
                 <h1> Github Battle: battle your friends.. and stuff. </h1>
-
                 <Link className='button' to='/battle'>Battle </Link>
             </div>
         )
